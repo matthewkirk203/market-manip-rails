1d1d4ece5ece691b7478454e9b051d5da1630c6c
Matthew Kirk <matthewkirk203@gmail.com>
2025-11-04 19:21:26 -0700

    add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_04_061943) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_05_021816) do
   create_table "listings", force: :cascade do |t|
     t.string "resource_name"
     t.integer "quantity"
@@ -41,6 +41,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_04_061943) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
