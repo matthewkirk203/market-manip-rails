81012660f8ef0fe17d4a5a213c15e1ac1202161d
Matthew Kirk <matthewkirk203@gmail.com>
2025-11-24 20:27:01 -0700

    add listing validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :resource_type
+
+  validates :price, numericality: { greater_than: 0 }
+  validates :quantity, numericality: { greater_than: 0 }
 end
